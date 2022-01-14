e6fafeaefe663db211f6d9eccd685a1a30475a73
babinicz <babinicz@onet.pl>
2022-01-14 18:22:58 +0100

    dodałem ciasteczka

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Blogs from "./components/Blogs";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
 import scrollreveal from "scrollreveal";
+import CookieConsent from "react-cookie-consent";
 
 function App() {
   useEffect(() => {
@@ -50,6 +51,33 @@ function App() {
       <BuySell />
       <Blogs />
       <Footer />
+      <CookieConsent
+        debug={true}
+        location="bottom"
+        style={{
+          background: "#333",
+          textAlign: "left",
+          paddingBottom: "1rem",
+          fontSize: "16px",
+          fontFamily: "Gideon Roman",
+        }}
+        buttonStyle={{
+          color: "#333",
+          background: "#fff",
+          fontSize: "18px",
+          fontFamily: "Gideon Roman",
+          marginRight: "1rem",
+        }}
+        buttonText="OK, rozumiem"
+        expires={365}
+      >
+        "W ramach naszej witryny stosujemy pliki cookies w celu świadczenia
+        Państwu usług na najwyższym poziomie, w tym w sposób dostosowany do
+        indywidualnych potrzeb. Korzystanie z witryny bez zmiany ustawień
+        dotyczących cookies oznacza, że będą one zamieszczane w Państwa
+        urządzeniu końcowym. Możecie Państwo dokonać w każdym czasie zmiany
+        ustawień dotyczących cookies."
+      </CookieConsent>
     </div>
   );
 }
